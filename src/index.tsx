718ce69a40223879c11193bf6b0c73228f83ce13
Raghib Hasan <raghibmidhat@gmail.com>
2018-11-19 21:53:14 +0200

    init

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+import { withClientState } from 'apollo-link-state';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
+
+const App = () => <div>Hello world</div>;
+
+render(<App />, document.getElementById('root') as HTMLElement);
